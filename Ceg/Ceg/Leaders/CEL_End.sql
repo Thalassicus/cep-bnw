9b01ffabdad1c3437fd74a4f4d633497ac4c9d7a
Thalassicus <Thalassicus@hotmail.com>
2013-12-14 04:11:35 -0600

    Rearranged leader data

--- Ceg/Ceg/Leaders/CEL_End.sql
+++ Ceg/Ceg/Leaders/CEL_End.sql
@@ -253,6 +253,113 @@ INSERT INTO Unit_FreePromotions
 		(UnitType, PromotionType)
 SELECT	'UNIT_ARABIAN_CAMELARCHER', 'PROMOTION_DESERT_POWER';
 
+
+
+	
+--
+-- Start Units
+--
+
+
+UPDATE Eras SET StartingDefenseUnits = StartingDefenseUnits - 1;
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_SCOUT', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_MONGOL'		,
+	'CIVILIZATION_RUSSIA'		,
+	'CIVILIZATION_SHOSHONE'		,
+	'CIVILIZATION_ZULU'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_BIREME', 'UNITAI_EXPLORE_SEA', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_CARTHAGE'		,
+	'CIVILIZATION_VENICE'		
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_TRIREME', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_ROME'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_ARCHER', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_BABYLON'		,
+	'CIVILIZATION_INCA'			,
+	'CIVILIZATION_MAYA'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_SPEARMAN', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_CELTS'		,
+	'CIVILIZATION_GREECE'		,
+	'CIVILIZATION_PERSIA'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_SWORDSMAN', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_IROQUOIS'		
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_CHARIOT_ARCHER', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_EGYPT'		,
+	'CIVILIZATION_HUNS'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_WORKER', 'UNITAI_WORKER', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_INDIA'		,
+	'CIVILIZATION_KOREA'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_GREAT_GENERAL', 'UNITAI_GENERAL', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_CHINA'
+);
+
+INSERT INTO Civilization_FreeUnits (UnitClassType, UnitAIType, Count, CivilizationType)
+SELECT 'UNITCLASS_WARRIOR', 'UNITAI_EXPLORE', 1, Type 
+FROM Civilizations WHERE Type IN (
+	'CIVILIZATION_AMERICA'		,
+	'CIVILIZATION_ARABIA'		,
+	'CIVILIZATION_AUSTRIA'		,
+	'CIVILIZATION_ASSYRIA'		,
+	'CIVILIZATION_AZTEC'		,
+	'CIVILIZATION_BRAZIL'		,
+	'CIVILIZATION_BYZANTIUM'	,
+	'CIVILIZATION_CARTHAGE'		,
+	'CIVILIZATION_CHINA'		,
+	'CIVILIZATION_DENMARK'		,
+	'CIVILIZATION_ENGLAND'		,
+	'CIVILIZATION_ETHIOPIA'		,
+	'CIVILIZATION_FRANCE'		,
+	'CIVILIZATION_GERMANY'		,
+	'CIVILIZATION_INDONESIA'	,
+	'CIVILIZATION_JAPAN'		,
+	'CIVILIZATION_MOROCCO'		,
+	'CIVILIZATION_NETHERLANDS'	,
+	'CIVILIZATION_OTTOMAN'		,
+	'CIVILIZATION_POLAND'		,
+	'CIVILIZATION_POLYNESIA'	,
+	'CIVILIZATION_PORTUGAL'		,
+	'CIVILIZATION_ROME'			,
+	'CIVILIZATION_SIAM'			,
+	'CIVILIZATION_SONGHAI'		,
+	'CIVILIZATION_SPAIN'		,
+	'CIVILIZATION_SWEDEN'		,
+	'CIVILIZATION_VENICE'		
+);
+
 		
 --
 -- Other
