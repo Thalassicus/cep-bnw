0201c4e9fe7102dc302ac93e97bcf89afc797dd8
GrantSP <fr00gYl@gmail.com>
2014-02-11 09:31:04 +1100

    Loading checks
    Changed the check to look for the actual name.

--- Cat/Cat/_Project/CAT_Start.sql
+++ Cat/Cat/_Project/CAT_Start.sql
@@ -31,7 +31,7 @@ INSERT INTO LoadedFile(Type, Value) VALUES ('Cat_Options.sql'		, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('Cat_Promotions.sql'	, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('Cat_PromotionOrder.sql'	, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('Cep_Options.sql'		, 0);
-INSERT INTO LoadedFile(Type, Value) VALUES ('MT_AlterTables.sql'	, 0);
+INSERT INTO LoadedFile(Type, Value) VALUES ('CAT_AlterTables.sql'	, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('YL_Data.xml'			, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('TW_BuildingStats.sql'	, 0);
 INSERT INTO LoadedFile(Type, Value) VALUES ('TW_PromoStats.sql'		, 0);
