078886f8e347384512fca62d4b4eb713ffd6d245
GrantSP <fr00gYl@gmail.com>
2014-02-18 12:21:50 +1100

    House cleaning
    Removed unused files and added the English versions into each language
    folder ready for translating.

---
+++ Cat/Cat/Text/PL_PL/YieldLibrary.sql
@@ -0,0 +1,104 @@
+--· EN_US/YieldLibrary.sql
+/*  Yield Breakdown Tooltip */
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CULTURE_WONDER_BONUS', '+{1_Num}% from having a World Wonder', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CULTURE_CITY_MOD', '+{1_Num}% from City', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CULTURE_PLAYER_MOD', '+{1_Num}% from Empire', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_FOOD_USAGE', '([COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] - [COLOR_NEGATIVE_TEXT]{2_Num}[ENDCOLOR] eaten by [ICON_CITIZEN] Citizens)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_FOODMOD_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% from Empire', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_FOODMOD_CAPITAL', '[NEWLINE][ICON_BULLET]{1_Num}% for [ICON_CAPITAL] Capital', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_FOODMOD_UNHAPPY', '[NEWLINE][ICON_BULLET][COLOR_WARNING_TEXT]{1_Num}%[ENDCOLOR] from [ICON_HAPPINESS_4] Unhappiness', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_FOODMOD_WLTKD', '[NEWLINE][ICON_BULLET]{1_Num}% from We Love the King Day', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_FOOD_CONVERSION', '[NEWLINE][ICON_BULLET]+{1_Num} [ICON_PRODUCTION] from excess [ICON_FOOD] Food', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_RAILROAD_CONNECTION', '[NEWLINE][ICON_BULLET]{1_Num} from Railroad Connection', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_DOMAIN', '[NEWLINE][ICON_BULLET]{1_Num}% for Unit Domain', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_COMBAT_TYPE', '[NEWLINE][ICON_BULLET]{1_Num}% for Combat Class', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_MILITARY', '[NEWLINE][ICON_BULLET]{1_Num}% for Military Units', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_WITH_BUILDING', '[NEWLINE][ICON_BULLET]{1_Num}% for {2_BuildingName}', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_WONDER_CITY', '[NEWLINE][ICON_BULLET]{1_Num}% for Wonders (City)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_WONDER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Wonders (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_WONDER_LOCAL_RES', '[NEWLINE][ICON_BULLET]{1_Num}% for Wonders ({2_ResourceName})', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_BUILDING_CITY', '[NEWLINE][ICON_BULLET]{1_Num}% for Buildings', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_CAPITAL_BUILDING_TRAIT', '[NEWLINE][ICON_BULLET]{1_Num}% from Buildings in [ICON_CAPITAL] Capital (Trait)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_SPACE', '[NEWLINE][ICON_BULLET]{1_Num}% for Spaceship (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_SUPPLY', '[NEWLINE][ICON_BULLET]{1_Num}% for Unit Supply (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_MILITARY_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Military Units (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_SETTLER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Settlers (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_CAPITAL_SETTLER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Settlers (Capital)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_RELIGION_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Religious Builds (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_COBMAT_CLASS_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Combat Class (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_UNIT_TRAIT', '[NEWLINE][ICON_BULLET]{1_Num}% for Unit Type (Trait)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_BUILDING_TRAIT', '[NEWLINE][ICON_BULLET]{1_Num}% for Buildings (Trait)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_WONDER_POLICY', '[NEWLINE][ICON_BULLET]{1_Num}% for Wonders (Policy)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_BUILDING_POLICY', '[NEWLINE][ICON_BULLET]{1_Num}% for Buildings (Policy)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_WORLD_WONDER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for World Wonders (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_TEAM_WONDER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Team Wonders (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_NATIONAL_WONDER_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for National Wonders (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_BUILDING_POLICY_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Buildings (Policy Empire Mod)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_SPACE_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Spaceship (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_SPECIALIST_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% for Specialists (Empire)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD', '[NEWLINE][ICON_BULLET]{1_Num}% from City', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_RESOURCES', '[NEWLINE][ICON_BULLET]{1_Num}% from Resources', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_HAPPINESS', '[NEWLINE][ICON_BULLET]{1_Num}% from Happiness', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_AREA', '[NEWLINE][ICON_BULLET]{1_Num}% from Area', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_PLAYER', '[NEWLINE][ICON_BULLET]{1_Num}% from Misc', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_CAPITAL', '[NEWLINE][ICON_BULLET]{1_Num}% for [ICON_CAPITAL] Capital', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_GOLDEN_AGE', '[NEWLINE][ICON_BULLET]{1_Num}% from Golden Age', '', '');
+/*  Misc */
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_BREAKDOWN', '[{1_color}]{2_stored}[ENDCOLOR]/[{1_color}]{3_needed}[ENDCOLOR] ([{1_color}]+{4_rate}[ENDCOLOR] per turn) {5_icon} {6_desc}', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TOPIC_LAW', 'Law', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TOPIC_POPULATION', 'Citizen', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TOPIC_CS_MILITARY', 'points per turn ', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PER_CITY', 'per city', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_SPLIT_AMONG_CITIES', 'split among your cities', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_MILITARY_UNIT_REWARDS', 'Military Unit', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_SCIENCE_FROM_HAPPINESS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}%[ENDCOLOR] [ICON_RESEARCH] from {1_Num} [ICON_HAPPINESS_1] Happiness.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_SCIENCE_FROM_UNHAPPINESS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]-{1_Num}%[ENDCOLOR] [ICON_RESEARCH] from {1_Num} [ICON_HAPPINESS_4] Unhappiness.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_GOLD_FOR_FREE', '{1_Num} for free.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_GOLD_FROM_OPEN_BORDERS', '{1_Num} from mutual Open Borders with other leaders.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_HAPPINESS_CITYSTATES', '{1_Num} National Happiness from City-States.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_HAPPINESS_DIFFICULTY_LEVEL', '{1_Num} National Happiness for free.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_SCIENCE_FROM_RESEARCH_AGREEMENTS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] [ICON_RESEARCH] from Research Agreements.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_LUXURIES', '{1_Num} from Luxury Resources.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_GOLD_FROM_POLICIES', '{1_Num} from Policies.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_FROM_POLICIES', '{1_Num} {2_icon} from Policies', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_FROM_TRAITS', '{1_Num} {2_icon} from Traits', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_FROM_PROCESSES', '{1_Num} {2_IconString} from Process', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_FROM_MINOR_CIVS', '{1_num} {2_icon} from City-States', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_FOCUS', '{2_icon} City Focus: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_POPULATION', '[ICON_CITIZEN] Population: {1_num}', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_NOT_CONNECTED', '[ICON_CONNECTED] Not Connected: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_RAZING', '[ICON_RAZING] Razing: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_RESISTANCE', '[ICON_RESISTANCE] Resistance: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_PUPPET', '[ICON_PUPPET] Puppet: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_OCCUPIED', '[ICON_OCCUPIED] Occupied: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_BLOCKADED', '[ICON_BLOCKADED] Blockaded: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_CAPITAL', '[ICON_CAPITAL] Capital: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_AVOID', '[ICON_MUSHROOM] Avoid Growth: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_IS_AVOID_MANY', '(More than {1_num}% of cities on avoid)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_CITYSTATE_MODIFIER_WEIGHT_TOTAL', '{1_num} (city) / {2_num} (empire)={3_num}% of {4_num}{5_icon}', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_USAGE', '([COLOR_POSITIVE_TEXT]{1_num}[ENDCOLOR] - [COLOR_NEGATIVE_TEXT]{2_num}[ENDCOLOR] consumed)', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_SURPLUS', '{1_num} {2_icon} Surplus', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_FROM_OTHER_YIELDS', '({1_num} {2_icon} + {3_num} {4_icon} {5_yieldstring})', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_SETTLER_POLICY', '[NEWLINE][ICON_BULLET]Policy Modifier for Settlers: {1_num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_POLICY', '[NEWLINE][ICON_BULLET]Policy Modifier: {1_Num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_BUILDINGS', '[NEWLINE][ICON_BULLET]Building Modifier: {1_Num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_CAPITAL_BUILDING_TRAIT', '[NEWLINE][ICON_BULLET]Modifier for Buildings in [ICON_CAPITAL] Capital: {1_Num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_YIELD_MOD_GLOBAL_BUILDINGS', '[NEWLINE][ICON_BULLET]Buildings in Other Cities: {1_Num}%', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_PRODMOD_YIELD_HAPPINESS', '[NEWLINE][ICON_BULLET][COLOR_WARNING_TEXT][ICON_HAPPINESS_4] Unhappiness Modifier: {1_Num}%[ENDCOLOR]', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FOR_FREE', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} for free.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_CITIES', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} from Cities.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_POLICIES', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} from Policies', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_MINORS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} from City-States.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_HAPPINESS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} from excess [ICON_HAPPINESS_1] Happiness.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_YIELD_FROM_RESEARCH_AGREEMENTS', '[ICON_BULLET][COLOR_POSITIVE_TEXT]{1_Num}[ENDCOLOR] {2_icon} from Research Agreements with other players.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_TP_SCIENCE', '[ICON_RESEARCH] Science being produced empire-wide towards the current research project.', '', '');
+INSERT INTO Cep_Language_EN_US (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-18'), 'TXT_KEY_NEXT_POLICY_TURN_LABEL', '{1_Num: number #} Turn(s) to Next Policy', '', '');
+
+
+
+
+
+
+
+
+UPDATE LoadedFile SET Value=1 WHERE Type='Cep_YieldLibrary.sql';
