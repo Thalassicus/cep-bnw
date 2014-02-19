ad88cc446dae8928cf61460f63b2b8307972f476
GrantSP <fr00gYl@gmail.com>
2014-02-20 09:09:59 +1100

    Temporary setup for the Map
    Added in a check for language to the lua and placed all the simple
    "isles" text into one file.

---
+++ CepMap/CepMap/Localization/Language.sql
@@ -0,0 +1,71 @@
+
+-- English
+
+UPDATE Language_EN_US SET Text = 'Isles' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_EN_US SET Text = 'Isles' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_EN_US SET Text = 'An island is any piece of sub-continental land that is surrounded by water. Very small islands can be called isles, cays or keys.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- German
+
+UPDATE Language_DE_DE SET Text = 'Inseln' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_DE_DE SET Text = 'Inseln' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_DE_DE SET Text = 'Eine Insel ist ein Stück Unter kontinentales Land, das von Wasser umgeben ist. Sehr kleine Inseln können aufgerufen werden Inseln, Inselchen oder Schlüssel.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Spanish
+
+UPDATE Language_ES_ES SET Text = 'Islas' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_ES_ES SET Text = 'Islas' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_ES_ES SET Text = 'Una isla es cualquier pedazo de tierra sub-continental que sea rodeada por el agua. Las islas muy pequeñas pueden ser llamados islas, cayos o llaves.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- French
+
+UPDATE Language_FR_FR SET Text = 'Îles' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_FR_FR SET Text = 'Îles' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_FR_FR SET Text = 'Une île est une pièce de terre sous-continentale qui est entourée par l''eau. Très petites îles peuvent être appelées îles, îlots ou des clés.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Italian
+
+UPDATE Language_IT_IT SET Text = 'Isole' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_IT_IT SET Text = 'Isole' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_IT_IT SET Text = 'Un''isola è un qualsiasi pezzo di terra sub-continentale, che è circondato da acqua. Molto piccole isole possono essere chiamati isole, isolotti o chiavi.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Japanese
+
+UPDATE Language_JA_JP SET Text = 'アイルズ' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_JA_JP SET Text = 'アイルズ' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_JA_JP SET Text = '島は水に囲まれている亜大陸の土地のどの部分である。非常に小さな島が島々、岩礁またはキーを呼び出すことができます。'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Korean
+
+UPDATE Language_KO_KR SET Text = '제도' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_KO_KR SET Text = '제도' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_KO_KR SET Text = '섬은 바다로 둘러싸여 서브 대륙 땅의 조각이다. 아주 작은 섬은 섬, 암초 또는 키를 호출 할 수 있습니다.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Polish
+
+UPDATE Language_PL_PL SET Text = 'Isles' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_PL_PL SET Text = 'Isles' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_PL_PL SET Text = 'Wyspa jest każdy kawałek ziemi pod-kontynentalnego, który jest otoczony przez wody. Bardzo małe wyspy można nazwać Isles, Cays lub klucze.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Russian
+
+UPDATE Language_RU_RU SET Text = 'Острова' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_RU_RU SET Text = 'Острова' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_RU_RU SET Text = 'Остров любой кусок суб-материковой суши, окруженной водой. Очень небольшие острова можно назвать островов, рифов или ключи.'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
+-- Traditional Chinese
+
+UPDATE Language_ZH_HANT_HK SET Text = '小岛' WHERE Tag = 'TXT_KEY_FEATURE_ATOLL';
+UPDATE Language_ZH_HANT_HK SET Text = '小岛' WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL';
+UPDATE Language_ZH_HANT_HK SET Text = '一个岛是任何一件是四面环水次大陆的土地。非常小的岛屿可以被称为岛屿，珊瑚礁或钥匙。'
+WHERE Tag = 'TXT_KEY_CIV5_FEATURES_ATOLL_TEXT';
+
