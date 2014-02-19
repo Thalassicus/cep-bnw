52c61059f9c82d6d8a82acefb43d78d8bf08165d
Thalassicus <Thalassicus@hotmail.com>
2014-02-19 16:42:57 -0600

    Leader updates

--- Ceg/Ceg/Text/FR_FR/Leaders.sql
+++ Ceg/Ceg/Text/FR_FR/Leaders.sql
@@ -1,1 +1,3 @@
 --·
+
+INSERT INTO Cep_Language_FR_FR (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-01-19'), 'TXT_KEY_TRAIT_ART_OF_WAR', 'You get an extra Spy. Great Generals see farther and give more strength.[NEWLINE]Starts with a Warrior and Great General. ', '', '');
