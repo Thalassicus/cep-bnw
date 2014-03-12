f48ff79e5677e87ace838930bf6e83ff2639932d
GrantSP <fr00gYl@gmail.com>
2014-03-13 08:44:41 +1100

    Policies.sql & Research.sql & Stories.sql
    More text added.
    Opportunities are renamed to Stories

--- Ceg/Ceg/Text/KO_KR/Research.sql
+++ Ceg/Ceg/Text/KO_KR/Research.sql
@@ -1,1 +1,36 @@
---·
+/* The sql commands and text below were formulated in the Research tab of the Google Spreadsheet 'Cep_Language'.
+https://docs.google.com/spreadsheets/d/1ptQRfVsW7UT_8gPexioizS31sM7K_3eBT3tjr4jruTs/edit#gid=1194171600
+If you make any changes be sure to update the spreadsheet also.*/
+
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRAINING', '훈련', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRAINING_PEDIA', '군사 훈련은 군인의 기능을 설정하고 개선하는 과정입니다. 군사 교육은 자발적 또는 강제적 인 의무가 될 수 있습니다. [NEWLINE] 신병 훈련은 효과적인 서비스의 회원이어야하는 데 필요한 기술의 기본 정보와 교육을 가르 칠하려고합니다. 이를 위해 서비스 회원은 기술적으로 정신적으로, 육체적으로 뚫고있다. [NEWLINE] 기본 훈련을 마친 후, 많은 장병들이 선택 또는 할당 특선 라인에서 고급 교육을 받고있다. 고급 훈련, 군사 기술과 장비는 종종 진행됩니다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRAINING_QUOTE', '[NEWLINE] [TAB] [TAB] "징병 필요하지만 반드시 징병."[NEWLINE] [TAB] [TAB] - 윌리엄 리옹 매켄지 킹 [NEWLINE]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_POLITICAL_SCIENCE', '정치', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_POLITICAL_SCIENCE_PEDIA', '이탈리아 르네상스 동안, 니콜로 마키아 벨리는 정치 제도와 배우의 직접 관찰을 현대 정치학의 중점을 설립했다. [NEWLINE] [NEWLINE] 볼테르, 루소, 토머스 홉스와 존 로크와 같은 나중에 유럽 철학자가 재료 과학의 원리를 적용 사회 과학을 만드는 사회 전체. 이들은 정부와 규율, 교회와 국가의 분리에 대한 요구, 그리고 왕의 신성한 권리의 점진적 감소하는 사이에 사회 계약을 강조 정책에 대한 방법을 용이하게했다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_POLITICAL_SCIENCE_QUOTE', '[NEWLINE] [TAB] [TAB] "정치는 도덕과 아무 관계가 없다."[NEWLINE] [TAB] [TAB] - 니콜로 마키아 벨리 [NEWLINE] [TAB]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRADE_TITLE', '무역', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRADE_DESC', '전시회 선사 시대에 통신의 시작과 유래. 그것은 일반적인 호기심, 창의력, 그리고 언어의 학습을 가했다. 이 engendered 있도록, 무역과 전문화는 인간의 의식과 네안데르탈 인의 종을 통해 인간의 ''승리의 성장을 가속화 적립 한 바와 같이. [NEWLINE] [NEWLINE] 석기 시대 흑요석과 부싯돌의 교환의 증거가있다. 이집트는 기원전 3000 년부터 보석을 만드는 데 사용되는 재료에 거래되었다. 메소포타미아의 수메르 인이 인더스 계곡의있는 Harappan 문명과 거래 할 때 장거리 무역로 처음 등장했다. 페니키아 인은 - 바다 상인을 언급 - 지중해에 걸쳐 여행하고, 멀리 북쪽 영국으로 청동을 제조하는 주석의 소스. 그들은 무역 엠포 리아라는 그리스인의 식민지 설립. [NEWLINE] [NEWLINE] 전시회 오랫동안 고대 지중해 해상 제국의 붕괴 후 탐구하는 인간의 경향을 운전을 계속했다. 더 멀리 콜럼버스가 바다를 넘어 전에 인도, 아라비아와 동 아프리카에 도달 청 그는 거대한 함대와 장거리 무역 확산.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_TRADE_QUOTE', '[NEWLINE] [TAB] [TAB] "어떤 이유는 인류의 선사 시대 뜨거운 경쟁의 존재 또는 경제의 잠재적 인 영향 중 하나를 할인하는 오늘날 존재하지 않습니다."[NEWLINE] [TAB] [TAB] - 제이슨 Shogren [NEWLINE] [ TAB]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_DIPLO_NEED_WRITING_TT', '[NEWLINE] [NEWLINE] 어느 선수는 아직이 항목을 (거래)을 거래 할 수있는 기술을 보유하고 있습니다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_CARVEL_HULLS', '카벨 헐', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_CARVEL_HULLS_PEDIA', '카벨의 선박 설계는 현재까지 1400 서유럽 해군의 힘의 지배를 가능하게 한 중요한 발전 중 하나였다. 카벨 선체의 나무 널빤지 대신 클링커 선체의 중복 판자 디자인의 가장자리에 서로 가장자리에 줄을. 이 강한 선체 디자인은 훨씬 더 큰 배와 돛을 지원합니다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_CARVEL_HULLS_QUOTE', '[NEWLINE] [TAB] [TAB] "배는 항구에 안전하지만, 그 배송을 위해 무엇을하지 않습니다."[NEWLINE] [TAB] [TAB] - 윌리엄 셰드 [NEWLINE]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_MEDIA_TITLE', '매스 미디어', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_MEDIA_DESC', '용어 "매스 미디어는"라디오의 출현으로 1920 년대에 만들어졌다, 그러나 많은 청중 소비에 대한 미디어의 아이디어는 고대 세계의 드라마와 연극 이후 주변왔다. 매스 미디어는 직접 출판 기술과 더 큰 가능한 관객에 도달하는 선도 통신에서 더 큰 진보, 기술에 의해 주도되었다. [NEWLINE] [NEWLINE] 원래 그냥 라디오, 신문, 잡지, 대중 매체 지금 포함 같은 통신 장치에 적용 텔레비전, 영화, 음악, 문고판 책, 비디오 게임, 인터넷. 이 글로브에 걸쳐 미디어 유형의 출현은 결코 전에 항상 좋은 방법처럼 대중에 도달 할 수있는 능력을 생겨났다으로. 하나 nefariously 마음 선전 또는 사실에 주입되는 더 많은 무고한 개인 정보를 통해 정보의 바이어스 또는 제어 다량하려는 시도가 처리하는 일부 정부 및 조직에 큰 문제가되고있다. 침략적인 광고 기법은 도처에 소비자의 등쌀에, 또한 대중 매체의 부산물이다. 메시지가 가치가있을 수 있으며, 일반적으로 사람이 정보를 사용할 수의 집중 출력에 들어지고, 자신의 아이디어와 작품을 게시하는 것이 점점 더 쉬워 동안 정말 건초 더미에있는 속담 바늘을 찾고처럼 될 수 있습니다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_MEDIA_QUOTE', '[NEWLINE] [TAB] [TAB] "통신의 속도가 보라 경이로웠다 그것은 속도가 우리가 사실로 알고있는 정보의 분포를 증식 할 수 있다는 사실이다.."[NEWLINE] [TAB] [TAB] - 에드워드 R. 머로우 [NEWLINE] [TAB] [TAB] [TAB]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_PRODUCTION_TITLE', '양산', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_PRODUCTION_DESC', '현대 대량 생산 조립 라인에 대한 표준화 된 제품의 대량의 구성이다. 대량 생산을위한 중요한 성분은 산업 기계, 교환 부품, 과학적 관리, 전력이다. [NEWLINE] [NEWLINE] 대량 생산이에 잘 전기 모터를 도입 헨리 포드의 포드 자동차 회사에 의해 1910 년대와 1920 년대에 대중화되었다 체인 또는 연속 생산의 공지 된 기술. 포드는 또한 구입 또는 설계 및 특수 목적의 공작 기계 및 설비를 건설했다. 이러한 기계들은 모두 생산 흐름에서 조직적으로 배열했다. 포드 관리는 작업자의 움직임을 최소화에 초점을 맞추고, 자신의 공장 과정을 기계화하기 위해 시간 연구와 실험을 수행 하였다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_MASS_PRODUCTION_QUOTE', '[NEWLINE] [TAB] [TAB] "이 실업을위한 하나의 규칙은 그는 다음과 같습니다. 가장 높은 임금을 지불 가능한 최저 비용으로 가능한 제품의 최고 품질을 확인합니다"[NEWLINE] [TAB] [TAB] - 헨리 포드 [NEWLINE] [TAB]', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_ION_PROPULSION_TITLE', '이온 추진', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_ION_PROPULSION_DESC', '공상 과학 소설 작가에 대한 선택의 추진은 NASA의 과학자와 엔지니어를위한 선택의 추진력이되고있다. 연료 및 전력의 이온 추진 시스템의 효율적인 사용을 빨리, 더 멀리 여행을 현대적인 우주선을 활성화하고 현재 다른 ​​추진 기술을보다 저렴합니다. 이온 추력기는 현재 통신 위성에 stationkeeping과 깊은 우주 탐사선에 대한 주요 추진을 위해 사용됩니다. 이온 로켓은 추력을 생성하는 이온을 추방하고 현재 다른 ​​로켓보다 높은 우주선의 최고 속도를 제공 할 수 있습니다. [NEWLINE] 이온은 단순히 전기적으로 대전 된 원자 또는 분자이다. 이온 추력기에서 이온이 정전기의 힘에 의해 가속된다. 가속에 사용되는 전기 분야는 스러 스터의 하류 측에 위치 전극에 의해 생성된다. [NEWLINE] 현대 이온 로켓은 추진제에 불활성 가스를 사용합니다. 로켓의 대부분은 화학적으로 불활성 무색, 무취, 무미, 이는 크세논 사용합니다. 추진제 추력기의 하류 단부로부터 주입 및 상류 단부를 향해 흐른다.', '', '');
+REPLACE INTO Language_KO_KR (Tag, Text, Gender, Plurality) VALUES ('TXT_KEY_TECH_ION_PROPULSION_QUOTE', '[NEWLINE] [TAB] [TAB] "모든 문명은 우주 여행 또는 멸종 중 하나가된다."[NEWLINE] [TAB] [TAB] - 칼 세이건 (Carl Sagan) [NEWLINE] [TAB]', '', '');
+
+
+
+
+UPDATE Loaded File SET Value=1, KO_KR=1 Where Type='Research.sql';
+
+
+
+
+
