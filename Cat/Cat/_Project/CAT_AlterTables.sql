ed26580a01d67582a08892cbfb240f58d48dbf44
GrantSP <fr00gYl@gmail.com>
2014-03-13 14:32:28 +1100

    Added in Korean files & Promotions.sql
    None of the Korean files were set correctly as with the Promotions.sql
    files.
    Cleaned up some duplicates in the error checking

--- Cat/Cat/_Project/CAT_AlterTables.sql
+++ Cat/Cat/_Project/CAT_AlterTables.sql
@@ -6,18 +6,7 @@
 
 CREATE TABLE IF NOT EXISTS Cep(Type text NOT NULL UNIQUE, Value);
 INSERT INTO Cep (Type, Value) VALUES ('SkipFile', 1);
-/*
-CREATE TABLE IF NOT EXISTS Cep_Language_EN_US (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_DE_DE (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_ES_ES (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_FR_FR (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_IT_IT (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_JA_JP (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_PL_PL (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_RU_RU (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Cep_Language_ZH_CN (DateModified integer, Tag text, Text text, Gender text, Plurality text);
-CREATE TABLE IF NOT EXISTS Language_ZH_CN (ID integer PRIMARY KEY, Tag text, Text text, Gender text, Plurality text);
-*/
+
 CREATE TABLE IF NOT EXISTS
 	Personalities (
 	Type			text NOT NULL UNIQUE
